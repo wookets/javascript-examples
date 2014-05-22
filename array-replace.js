7d92e18db0563fbf1bf45eb162a8db414bf80120
Sean Wesenberg <wookets@gmail.com>
2014-05-22 09:57:05 -0500

    added array replace
    Signed-off-by: Sean Wesenberg <wookets@gmail.com>

---
+++ array-replace.js
@@ -0,0 +1,6 @@
+
+var items = ['red', 'green', 'blue'];
+
+items[0] = 'orange'; // replace 'red' with 'orange'
+
+console.log(items);
--- tilde.js
+++ tilde.js
@@ -18,7 +18,7 @@ console.log(~index);
 if (~index) { // ~ converts -1 into 0, which also means 'false' in javascript
   items[index] = 'yellow';
 }
-console.log(items.indexOf('purple') !== -1); // this is the same as the above ~ 
+console.log(items.indexOf('purple') !== -1); // this is the same as the above ~
 if (items.indexOf('purple') !== -1) {
   items[index] = 'yellow';
 }
