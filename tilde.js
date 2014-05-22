bf9e90c3afd7cd0804150f614a1abbf633a6f327
Sean Wesenberg <wookets@gmail.com>
2014-05-22 09:55:17 -0500

    added tilde example
    Signed-off-by: Sean Wesenberg <wookets@gmail.com>

---
+++ tilde.js
@@ -0,0 +1,25 @@
+
+var items = ['red','green','blue']
+console.log(items);
+
+var index = items.indexOf('green');
+console.log(index);
+
+console.log(~index);
+if (~index) {
+  items[index] = 'orange';
+}
+console.log(items);
+
+
+index = items.indexOf('purple');
+console.log(index);
+console.log(~index);
+if (~index) { // ~ converts -1 into 0, which also means 'false' in javascript
+  items[index] = 'yellow';
+}
+console.log(items.indexOf('purple') !== -1); // this is the same as the above ~ 
+if (items.indexOf('purple') !== -1) {
+  items[index] = 'yellow';
+}
+console.log(items);
