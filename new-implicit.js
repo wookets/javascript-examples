75c4cd499657b1ac90aebf70ab4265e9be977ac0
Sean Wesenberg <wookets@gmail.com>
2014-06-16 15:53:24 -0500

    implicit new
    Signed-off-by: Sean Wesenberg <wookets@gmail.com>

---
+++ new-implicit.js
@@ -0,0 +1,7 @@
+var x1 = {};           // new object
+var x2 = "";           // new primitive string
+var x3 = 0;            // new primitive number
+var x4 = false;        // new primitive boolean
+var x5 = [];           // new array object
+var	x6 = /()/          // new regexp object
+var x7 = function(){}; // new function object
